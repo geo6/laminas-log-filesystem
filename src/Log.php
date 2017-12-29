304a6be825c43786d41189443e11fde2be4502a8
jbelien <dev@jbelien.be>
2017-12-29 09:35:10 +0000

    Apply fixes from StyleCI

--- src/Log.php
+++ src/Log.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Geo6;
 
 use ErrorException;
@@ -7,76 +8,84 @@ use Zend\Log\Logger;
 use Zend\Log\Processor\PsrPlaceholder;
 use Zend\Log\Writer\Stream;
 
-class Log {
-  public static function write($fname, $text, $data = array(), $level = NULL) {
-    $directory = realpath(dirname($fname));
-    if (!file_exists($directory) || !is_dir($directory) || !is_writable($directory)) {
-      throw new ErrorException(sprintf('The directory "%s" is not a vaild directory to write log files.', $directory));
+class Log
+{
+    public static function write($fname, $text, $data = [], $level = null)
+    {
+        $directory = realpath(dirname($fname));
+        if (!file_exists($directory) || !is_dir($directory) || !is_writable($directory)) {
+            throw new ErrorException(sprintf('The directory "%s" is not a vaild directory to write log files.', $directory));
+        }
+
+        if (is_null($level)) {
+            $level = Logger::INFO;
+        }
+
+        // ---------------------------------------------------------------------------------------------
+        if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $data['_ip'] = $_SERVER['HTTP_X_FORWARDED_FOR'].(isset($_SERVER['REMOTE_ADDR']) ? ' ('.$_SERVER['REMOTE_ADDR'].')' : '');
+        } else {
+            $data['_ip'] = (isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : null);
+        }
+
+        // ---------------------------------------------------------------------------------------------
+        $data['_referer'] = (isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : null);
+
+        // ---------------------------------------------------------------------------------------------
+        if (!empty(ini_get('browscap')) && isset($_SERVER['HTTP_USER_AGENT'])) {
+            $gb = get_browser();
+            $data['_browser'] = ($gb->browser != 'Default Browser' ? $gb->platform.' '.$gb->parent : null);
+        } else {
+            $data['_browser'] = null;
+        }
+
+        // ---------------------------------------------------------------------------------------------
+        $auth = new AuthenticationService();
+        $login = $auth->getIdentity();
+
+        $data['_login'] = null;
+        if (isset($_SESSION['uid']) && !empty($_SESSION['uid'])) {
+            $data['_login'] = $login;
+        }
+
+        // ---------------------------------------------------------------------------------------------
+        $logger = new Logger();
+        $logger->addWriter(new Stream($fname));
+        $logger->addProcessor(new PsrPlaceholder());
+
+        $logger->log($level, $text, $data);
     }
 
-    if (is_null($level)) $level = Logger::INFO;
+    public static function read($fname)
+    {
+        $directory = realpath(dirname($fname));
+        if (!file_exists($directory) || !is_dir($directory) || !is_readable($directory)) {
+            throw new ErrorException(sprintf('The directory "%s" is not a vaild directory to read log files.', $directory));
+        }
 
-    // ---------------------------------------------------------------------------------------------
-    if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-      $data['_ip'] = $_SERVER['HTTP_X_FORWARDED_FOR'].(isset($_SERVER['REMOTE_ADDR']) ? ' ('.$_SERVER['REMOTE_ADDR'].')' : '');
-    } else {
-      $data['_ip'] = (isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : NULL);
-    }
+        $logs = [];
 
-    // ---------------------------------------------------------------------------------------------
-    $data['_referer'] = (isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : NULL);
-
-    // ---------------------------------------------------------------------------------------------
-    if (!empty(ini_get('browscap')) && isset($_SERVER['HTTP_USER_AGENT'])) {
-      $gb = get_browser();
-      $data['_browser'] = ($gb->browser != 'Default Browser' ? $gb->platform.' '.$gb->parent : NULL);
-    } else {
-      $data['_browser'] = NULL;
-    }
-
-    // ---------------------------------------------------------------------------------------------
-    $auth = new AuthenticationService();
-    $login = $auth->getIdentity();
-
-    $data['_login'] = NULL; if (isset($_SESSION['uid']) && !empty($_SESSION['uid'])) $data['_login'] = $login;
-
-    // ---------------------------------------------------------------------------------------------
-    $logger = new Logger;
-    $logger->addWriter(new Stream($fname));
-    $logger->addProcessor(new PsrPlaceholder);
-
-    $logger->log($level, $text, $data);
-  }
-
-  public static function read($fname) {
-    $directory = realpath(dirname($fname));
-    if (!file_exists($directory) || !is_dir($directory) || !is_readable($directory)) {
-      throw new ErrorException(sprintf('The directory "%s" is not a vaild directory to read log files.', $directory));
-    }
-
-    $logs = array();
-
-    $fp = fopen($fname, 'r');
-    if ($fp) {
-      while (($r = fgets($fp, 10240)) !== FALSE) {
-        // Zend\Log : %timestamp% %priorityName% (%priority%): %message% %extra%
-        if (preg_match('/^(.+) (DEBUG|INFO|NOTICE|WARN|ERR|CRIT|ALERT|EMERG) \(([0-9])\): (.+) ({.+})$/', $r, $matches) == 1) {
-          $logs[] = array(
+        $fp = fopen($fname, 'r');
+        if ($fp) {
+            while (($r = fgets($fp, 10240)) !== false) {
+                // Zend\Log : %timestamp% %priorityName% (%priority%): %message% %extra%
+                if (preg_match('/^(.+) (DEBUG|INFO|NOTICE|WARN|ERR|CRIT|ALERT|EMERG) \(([0-9])\): (.+) ({.+})$/', $r, $matches) == 1) {
+                    $logs[] = [
             'timestamp'     => strtotime($matches[1]), // ISO 8601
             'priority_name' => $matches[2],
             'priority'      => $matches[3],
             'message'       => $matches[4],
-            'extra'         => json_decode($matches[5], TRUE)
-          );
+            'extra'         => json_decode($matches[5], true),
+          ];
+                }
+                // Other (olg) logs
+                else {
+                    $logs[] = $r;
+                }
+            }
         }
-        // Other (olg) logs
-        else {
-          $logs[] = $r;
-        }
-      }
-    }
-    fclose($fp);
+        fclose($fp);
 
-    return $logs;
-  }
+        return $logs;
+    }
 }
