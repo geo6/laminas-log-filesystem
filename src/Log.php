f68cc50d69404f77991a1d292d331be71f937267
jbelien <dev@jbelien.be>
2019-09-27 15:16:52 +0000

    Apply fixes from StyleCI

--- src/Log.php
+++ src/Log.php
@@ -45,7 +45,7 @@ class Log
             $extra['_ip'] = $_SERVER['HTTP_X_FORWARDED_FOR'];
 
             if (isset($_SERVER['REMOTE_ADDR'])) {
-                $extra['_ip'] .= ' (' . $_SERVER['REMOTE_ADDR'] . ')';
+                $extra['_ip'] .= ' ('.$_SERVER['REMOTE_ADDR'].')';
             }
         } elseif (isset($_SERVER['REMOTE_ADDR'])) {
             $extra['_ip'] = $_SERVER['REMOTE_ADDR'];
@@ -72,8 +72,8 @@ class Log
                 $extra['_device'] = 'desktop';
             }
 
-            $extra['_platform'] = $agent->platform() . ' ' . $agent->version($agent->platform());
-            $extra['_browser'] = $agent->browser() . ' ' . $agent->version($agent->browser());
+            $extra['_platform'] = $agent->platform().' '.$agent->version($agent->platform());
+            $extra['_browser'] = $agent->browser().' '.$agent->version($agent->browser());
         }
 
         // ---------------------------------------------------------------------------------------------
